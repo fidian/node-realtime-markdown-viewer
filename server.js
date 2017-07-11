7a171495a02a40db0a67c546f018c968180e0137
Tyler Akins <fidian@rumkin.com>
2017-07-11 08:58:05 -0500

    Getting this to deploy again on Heroku

--- server.js
+++ server.js
@@ -11,10 +11,10 @@ app.use(express.static(__dirname + '/public'));
 
 // routes for app
 app.get('/', function(req, res) {
-  res.render('pad');
+    res.render('pad');
 });
 app.get('/(:id)', function(req, res) {
-  res.render('pad');
+    res.render('pad');
 });
 
 // get sharejs dependencies
@@ -24,16 +24,16 @@ var sharejs = require('share');
 var redisClient;
 console.log(process.env.REDISTOGO_URL);
 if (process.env.REDISTOGO_URL) {
-  var rtg   = require("url").parse(process.env.REDISTOGO_URL);
-  redisClient = require("redis").createClient(rtg.port, rtg.hostname);
-  redisClient.auth(rtg.auth.split(":")[1]);
+    var rtg   = require("url").parse(process.env.REDISTOGO_URL);
+    redisClient = require("redis").createClient(rtg.port, rtg.hostname);
+    redisClient.auth(rtg.auth.split(":")[1]);
 } else {
-  redisClient = require("redis").createClient();
+    redisClient = require("redis").createClient();
 }
 
 // options for sharejs 
 var options = {
-  db: {type: 'redis', client: redisClient}
+    db: {type: 'redis', client: redisClient}
 };
 
 // attach the express server to sharejs
